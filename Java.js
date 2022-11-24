09853bd039c507ae344b29c610226a73b2cce0bb
MIbmkz <s10242151@connect.np.edu.sg>
2022-11-24 21:32:08 +0800

    Changed htmls & perfected dropdown from prev fail

--- Java.js
+++ Java.js
@@ -2,3 +2,9 @@ window.addEventListener("beforeunload", function () {
     document.body.classList.remove("animate-in");
     document.body.classList.add("animate-out");
 });
+
+let subMenu = document.getElementById("subMenu");
+
+function toggleMenu(){
+    subMenu.classList.toggle("open-menu");
+}
